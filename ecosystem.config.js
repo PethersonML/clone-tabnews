dacfad07ba4a2fe09b7ecd7562ef6786ad409bdb
PethersonML <petherson.monteiro@hotmail.com>
2024-08-14 02:39:07 -0300

    Adiciona página inicial e arquivo de configuração do PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  apps : [{
+    name   : "clone-tabnews",
+    script : "npm run dev"
+  }]
+}
---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>Iniciando com Next.JS</h1>
+}
+
+export default Home;
